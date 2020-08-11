00b6946b9cda639dd500825ff29f6a1f0bf1697a
Menazina <menazinap@gmail.com>
2020-08-11 10:33:37 -0400

    lab-mysql

---
+++ module-1/lab-mysql/update.sql
@@ -0,0 +1,23 @@
+/*CREATE DATABASE Dentons;
+USE sales;*/
+/*CREATE TABLE invoices
+(
+number VARCHAR(8),
+client VARCHAR(12),
+matter VARCHAR(25),
+date DATE,
+timekeeper CHAR(3)
+PRIMARY KEY(number)
+);*/
+/*INSERT INTO invoices
+(number, client, matter, date, timekeeper)
+VALUES(3044, 'CIFI', 'Kaixo', '2019-03-29','Amanda Valdez');*/
+/*INSERT INTO invoices
+(number, client, matter, date, timekeeper)
+VALUES(4055, 'Total', 'General', '2020-06-21','Amanda'),
+ (8976, 'Fermaca', 'Gas Refinery', '2019-12-18','Rogelio'),
+ (5673, 'BAS', 'NAFIN', '2019-05-18','Jorge'),
+ (5982, 'BP', 'General', '2020-01-30','Amanda'),
+ (6563, 'CIFI',NULL, NULL);*/
+ 
+ 
