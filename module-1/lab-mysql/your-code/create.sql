c9ba131b781b05f46641fe4946cac9c324d40cb5
CarlosThomas95 <carlostomas33@gmail.com>
2020-08-13 20:20:18 -0500

    Version Final

---
+++ module-1/lab-mysql/your-code/create.sql
@@ -0,0 +1,48 @@
+/*CREATE DATABASE lab_mysql;*/
+/*USE lab_mysql;*/
+/*CREATE TABLE cars
+(
+ID INT(5),
+VIN VARCHAR(17),
+Manufacturer VARCHAR(10),
+Model VARCHAR(20),
+MYear INT(4),
+Color VARCHAR(10),
+PRIMARY KEY(ID)
+);*/
+/*CREATE TABLE customer
+(
+ID VARCHAR(5),
+CustomerID INT(10),
+Nombre VARCHAR(30),
+Phone VARCHAR(20),
+Email VARCHAR(30),	
+Address VARCHAR(30),
+City VARCHAR(15),
+State VARCHAR(20),
+Country VARCHAR(15),
+Postal INT(5),
+PRIMARY KEY(ID)
+);*/
+/*CREATE TABLE salesperson
+(
+ID INT(5),
+StaffID	INT(5),
+Nombre VARCHAR(30),
+Store VARCHAR(20),
+PRIMARY KEY(ID)
+);*/
+/*CREATE TABLE invoices
+(
+ID INT(5),
+Invoice_number INT(10),
+DT DATE,
+Car INT(5),
+Customer INT(5),
+Salesperson INT(5),
+PRIMARY KEY(ID)
+);*/
+/*SELECT *FROM cars;*/
+/*SELECT *FROM customer;*/
+/*SELECT *FROM invoices;*/
+/*SELECT *FROM salesperson;*/
---
+++ module-1/lab-mysql/your-code/updateanddelete.sql
@@ -0,0 +1,25 @@
+/*SELECT store FROM salesperson
+WHERE store = "Mimia";*/
+
+/*SET SQL_SAFE_UPDATES=0; PARA PODER ACTUALIZAR DE FORMA SEGURA LOS CAMBIOS EN LA DATABASE
+UPDATE salesperson
+SET store = "Miami"
+WHERE store = "Mimia";*/
+
+/*UPDATE customer
+SET Email = "ppicasso@gmail.com"
+WHERE Nombre = "Pablo Picasso";*/
+
+/*UPDATE customer
+SET Email = "lincoln@us.gov"
+WHERE Nombre = "Abraham Lincoln";*/   
+
+/*UPDATE customer
+SET Email = "hello@napoleon.me"
+WHERE Nombre = "Napoléon Bonaparte";*/
+
+/*SELECT Email FROM customer*/
+
+/*DELETE FROM cars
+WHERE ID = 4;
+SELECT *FROM cars*/
